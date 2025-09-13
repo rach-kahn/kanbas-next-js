fe789981036a16b3e87fbc8478230c9283e0af8d
rach-kahn <11rsoccer11@gmail.com>
2025-09-13 15:45:46 -0400

    through vscode terminal

--- kambaz-next-js-cs4550-fa25/app/Labs/Lab1/page.tsx
+++ kambaz-next-js-cs4550-fa25/app/Labs/Lab1/page.tsx
@@ -1,8 +1,201 @@
+"use client";
 export default function Lab1() {
     return (
       <div id="wd-lab1">
+        <h1>Labs</h1>
         <h2>Lab 1</h2>
+        <h3>HTML Examples</h3>
+          <div id="wd-h-tag">
+        <h4>Heading Tags</h4>
+Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
+      </div>
+       <div id="wd-p-tag">
+        <h4>Paragraph Tag</h4>
+        <p id="wd-p-1"> ... </p>
+        
+        <p id="wd-p-2">
+
+This is the first paragraph. The paragraph tag is used to format vertical gaps between long pieces of text like this one.
+        </p>
+        <p id="wd-p-3">
+
+This is the second paragraph. Even though there is a deliberate white gap between the paragraph above and this paragraph, by default browsers render them as one contiguous piece of text as shown here on the right.
+         </p>
+        <p id="wd-p-4">
+
+This is the third paragraph. Wrap each paragraph with the paragraph tag to tell browsers to render the gaps.
+         </p>
+         </div>
+         <div id="wd-lists">
+        <h4>List Tags</h4>
+        <h5>Ordered List Tag</h5>
+        How to make pancakes:
+        <ol id="wd-pancakes">
+        <li>Mix dry ingredients.</li>
+        <li>Add wet ingredients.</li>
+        <li>Stir to combine.</li>
+        <li>Heat a skillet or griddle.</li>
+        <li>Pour batter onto the skillet.</li>
+        <li>Cook until bubbly on top.</li>
+        <li>Flip and cook the other side.</li>
+        <li>Serve and enjoy!</li>
+        </ol>
+        My favorite recipe: 
+        <ol id="wd-your-favorite-recipe">
+          <li>Take your pizza crust and top with sauce.</li>
+          <li>Top with cheese and other toppings.</li>
+          <li>Bake in the oven at 350 degrees for 20 minutes.</li>
+          <li>Remove from oven and let cool before slicing and serving.</li>
+        </ol>
+        <h5>Unordered List Tag</h5>
+        My favorite books (in no particular order)
+<ul id="wd-my-books">
+  <li>Dune</li>
+  <li>Lord of the Rings</li>
+  <li>Ender's Game</li>
+  <li>Red Mars</li>
+  <li>The Forever War</li>
+</ul>
+Your favorite books (in no particular order)
+<ul id="wd-your-books">
+  <li>The Bombshell</li>
+  <li>What Lies in The Woods</li>
+  <li>Little Fires Everywhere</li>
+</ul>
+
+         
+      </div>
+       <div id="wd-tables">
+        <h4>Table Tag</h4>
+        <table border={1} width="100%">
+          <thead>
+            <tr>
+              <th>Quiz</th>
+              <th>Topic</th>
+              <th>Date</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Q1</td>
+              <td>HTML</td>
+              <td>2/3/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q2</td>
+              <td>CSS</td>
+              <td>2/10/21</td>
+              <td>90</td>
+            </tr>
+            <tr> 
+              <td>Q3</td>
+              <td>HTML</td>
+              <td>2/14/21</td>
+              <td>95</td>
+            </tr>
+            <tr> 
+              <td>Q4</td>
+              <td>XML</td>
+              <td>2/21/21</td>
+              <td>66</td>
+            </tr>
+            <tr> 
+              <td>Q5</td>
+              <td>JSON</td>
+              <td>3/01/21</td>
+              <td>90</td>
+            </tr>
+            <tr> 
+              <td>Q6</td>
+              <td>DOM</td>
+              <td>3/10/21</td>
+              <td>99</td>
+            </tr>
+            <tr> 
+              <td>Q7</td>
+              <td>APIs</td>
+              <td>3/20/21</td>
+              <td>90</td>
+            </tr>
+            <tr> 
+              <td>Q8</td>
+              <td>MongoDB</td>
+              <td>4/01/21</td>
+              <td>85</td>
+            </tr>
+            <tr> 
+              <td>Q9</td>
+              <td>Servers</td>
+              <td>4/14/21</td>
+              <td>82</td>
+            </tr>
+            <tr> 
+              <td>Q10</td>
+              <td>AJAX</td>
+              <td>5/11/21</td>
+              <td>98</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={3}>Average</td>
+              <td>88</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <div id="wd-images">
+  <h4>Image tag</h4>
+  Loading an image from the internet: <br />
+  <img id="wd-starship" width="400px"   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+  <br />
+  Loading a local image:
+  <br />
+  <img id="wd-teslabot" src="/images/teslabot.jpg" height="200px" />
+  </div>
+  <div id="wd-forms">
+  <h4>Form Elements</h4>
+  <form id="wd-text-fields">
+    <h5>Text Fields</h5>
+    <label htmlFor="wd-text-fields-username">Username:</label>
+    <input placeholder="jdoe" id="wd-text-fields-username" /> <br />
+    <label htmlFor="wd-text-fields-password">Password:</label>
+    <input type="password" value="123@#$asd" id="wd-text-fields-password" />
+    <br />
+    <label htmlFor="wd-text-fields-first-name">First name:</label>
+    <input type="text" title="John" id="wd-text-fields-first-name" /> <br />
+    <label htmlFor="wd-text-fields-last-name">Last name:</label>
+    <input type="text" placeholder="Doe"
+           value="Wonderland"
+           title="The last name"
+           id="wd-text-fields-last-name" />
+    {/* copy rest of form elements here  */}
+  </form>
+</div>
+<h5>Text boxes</h5>
+<label>Biography:</label><br/>
+<textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in tincidunt arcu, sit amet laoreet arcu. Donec eu lacus at nunc maximus iaculis. Sed tortor est, egestas sit amet interdum in, ultrices quis est. Sed facilisis neque at tellus vulputate, ac vestibulum risus consequat. Fusce sit amet eros id sem congue scelerisque non at nunc. Quisque lobortis vestibulum consequat. Vivamus felis tortor, efficitur faucibus nisl eu, posuere lacinia turpis. Quisque massa nisl, fermentum efficitur viverra pulvinar, tempus viverra sem. Curabitur quam dolor, pharetra vel fermentum a, malesuada in metus. Aliquam sit amet dapibus tortor. Vestibulum iaculis justo quis blandit vehicula.</textarea>
+
+<h5 id="wd-buttons">Buttons</h5>
+<button type="button"
+        onClick={() => alert("Life is Good!")}
+        id="wd-all-good">
+  Hello World!
+</button>
+
+
+
+
+
+
+
+
+
+
       </div>
     );
+    
   }
   
