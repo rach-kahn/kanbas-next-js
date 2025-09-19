10d34b04db76dffe2d8a0d2d7b625d497c9577d6
rach-kahn <11rsoccer11@gmail.com>
2025-09-19 15:41:47 -0400

    real final version

--- kambaz-next-js-cs4550-fa25/app/(Kambaz)/Courses/[cid]/Home/Status.tsx
+++ kambaz-next-js-cs4550-fa25/app/(Kambaz)/Courses/[cid]/Home/Status.tsx
@@ -3,11 +3,13 @@ export default function CourseStatus() {
     <div id="wd-course-status">
       <h2>Course Status</h2>
       <button>Unpublish</button> <button>Publish</button>
-      <button>Import Existing Content</button>
-      <button>Import from Commons</button>
-      <button>Choose Home Page</button>
-      <button>View Course Stream</button>
-      <button>New Announcement</button>
-      <button>New Analytics</button>
+      <br/>
+      <br/>
+      <button>Import Existing Content</button><br/>
+      <button>Import from Commons</button><br/>
+      <button>Choose Home Page</button><br/>
+      <button>View Course Stream</button><br/>
+      <button>New Announcement</button><br/>
+      <button>New Analytics</button><br/>
       <button>View Course Notifications</button>
     </div> );}
