c8d531e44cc12fc578b86f6f81ebdc5752dd2b51
rach-kahn <11rsoccer11@gmail.com>
2025-09-19 15:31:18 -0400

    fixed 404 and images

--- kambaz-next-js-cs4550-fa25/app/(Kambaz)/Dashboard/page.tsx
+++ kambaz-next-js-cs4550-fa25/app/(Kambaz)/Dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function Dashboard() {
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
           <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/reactjs.jpg" width={200} height={150} />
+            <Image src="/images/reactjs.jpg" alt="dash image" width={200} height={150} />
             <div>
               <h5> CS1234 React JS </h5>
               <p className="wd-dashboard-course-title">
@@ -20,7 +20,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course">
             <Link href="/Courses/4321" className="wd-dashboard-course-link">
-             <Image src="/images/reactjs.jpg" width={200} height={150} />
+             <Image src="/images/reactjs.jpg" alt="dash image" width={200} height={150} />
             <div>
               <h5> CS3500 OOD </h5>
               <p className="wd-dashboard-course-title">
@@ -32,7 +32,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course"> 
             <Link href="/Courses/2345" className="wd-dashboard-course-link">
-             <Image src="/images/reactjs.jpg" width={200} height={150} />
+             <Image src="/images/reactjs.jpg" alt="dash image" width={200} height={150} />
             <div>
               <h5> CS3000 Algo </h5>
               <p className="wd-dashboard-course-title">
@@ -44,7 +44,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course">
             <Link href="/Courses/6789" className="wd-dashboard-course-link">
-             <Image src="/images/reactjs.jpg" width={200} height={150} />
+             <Image src="/images/reactjs.jpg" alt="dash image" width={200} height={150} />
             <div>
               <h5> CS5000 Advanced OOD </h5>
               <p className="wd-dashboard-course-title">
@@ -56,7 +56,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course">
             <Link href="/Courses/9876" className="wd-dashboard-course-link">
-             <Image src="/images/reactjs.jpg" width={200} height={150} />
+             <Image src="/images/reactjs.jpg" alt="dash image" width={200} height={150} />
             <div>
               <h5> CS2500 Fundies2 </h5>
               <p className="wd-dashboard-course-title">
@@ -68,7 +68,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course">
             <Link href="/Courses/2468" className="wd-dashboard-course-link">
-             <Image src="/images/reactjs.jpg" width={200} height={150} />
+             <Image src="/images/reactjs.jpg" alt="dash image" width={200} height={150} />
             <div>
               <h5> CS2000 Fundies1 </h5>
               <p className="wd-dashboard-course-title">
@@ -80,7 +80,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course">
             <Link href="/Courses/1357" className="wd-dashboard-course-link">
-             <Image src="/images/reactjs.jpg" width={200} height={150} />
+             <Image src="/images/reactjs.jpg" alt="dash image" width={200} height={150} />
             <div>
               <h5> CS4000 AI </h5>
               <p className="wd-dashboard-course-title">
