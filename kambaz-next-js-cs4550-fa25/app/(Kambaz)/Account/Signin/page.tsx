46dadb153b8e0243bb563350cace4bde30d2a2d2
rach-kahn <11rsoccer11@gmail.com>
2025-09-19 00:04:09 -0400

    all done

--- kambaz-next-js-cs4550-fa25/app/(Kambaz)/Account/Signin/page.tsx
+++ kambaz-next-js-cs4550-fa25/app/(Kambaz)/Account/Signin/page.tsx
@@ -3,7 +3,7 @@ export default function Signin() {
  return (
    <div id="wd-signin-screen">
      <h3>Sign in</h3>
-     <input placeholder="username" className="wd-username" /> <br />
+     <input defaultValue="Rachel" placeholder="username" type="text" className="wd-username" /> <br />
      <input placeholder="password" type="password" className="wd-password" /> <br />
      <Link href="/Dashboard" id="wd-signin-btn"> Sign in </Link> <br />
      <Link href="Signup" id="wd-signup-link"> Sign up </Link>
--- kambaz-next-js-cs4550-fa25/app/(Kambaz)/Account/Signup/page.tsx
+++ kambaz-next-js-cs4550-fa25/app/(Kambaz)/Account/Signup/page.tsx
@@ -4,8 +4,8 @@ export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
+      <input placeholder="username" type="text" className="wd-username" /><br/>
+      <input defaultValue="password" placeholder="password" type="password" className="wd-password" /><br/>
       <input placeholder="verify password"
              type="password" className="wd-password-verify" /><br/>
       <Link  href="Profile" > Sign up </Link><br />
