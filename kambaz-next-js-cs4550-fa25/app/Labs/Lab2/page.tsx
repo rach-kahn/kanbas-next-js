88dc9849b923710150d8b570cf2c0652d3e30bc6
rach-kahn <11rsoccer11@gmail.com>
2025-10-03 22:46:08 -0400

    fixed errors

--- kambaz-next-js-cs4550-fa25/app/Labs/Lab2/page.tsx
+++ kambaz-next-js-cs4550-fa25/app/Labs/Lab2/page.tsx
@@ -29,7 +29,7 @@ export default function Lab2() {
         <h3>Styling with the STYLE attribute</h3>
         <p >
           Style attribute allows configuring look and feel
-          right on the element. Although it's very convenient
+          right on the element. Although it is very convenient
           it is considered bad practice and you should avoid
           using the style attribute
         </p>
@@ -40,7 +40,7 @@ export default function Lab2() {
             elements of the same name, e.g., P, we can refer to a specific element by its ID
           </p>
           <p id="wd-id-selector-2">
-            Here's another paragraph using a different ID and a different look and
+            Here is another paragraph using a different ID and a different look and
             feel
           </p>
         </div>
@@ -48,7 +48,7 @@ export default function Lab2() {
           <h3>Class selectors</h3>
 
           <p className="wd-class-selector">
-            Instead of using IDs to refer to elements, you can use an element's CLASS attribute
+            Instead of using IDs to refer to elements, you can use an elements CLASS attribute
           </p>
 
           <h4 className="wd-class-selector">
@@ -63,7 +63,7 @@ export default function Lab2() {
               Selectors can be combined to refer elements in particular
               places in the document
               <p className="wd-selector-3">
-                This paragraph's red background is referenced as
+                This paragraphs red background is referenced as
                 <br />
                 .selector-2 .selector3<br />
                 meaning the descendant of some ancestor.<br />
